33e7de6f3783f5f2633a11f65ae368e823b02774
Ryan Gregg <rgregg@microsoft.com>
2014-12-19 12:47:52 -0800

    Initial commit

---
+++ MarkdownScanner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MarkdownScanner
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var options = new ScannerOptions();
+			if (!CommandLine.Parser.Default.ParseArguments(args, options))
+			{
+				Console.WriteLine(options.UsageText);
+				return;
+			}
+
+			var scanner = new Scanner(options);
+			scanner.Scan();
+		}
+	}
+
+
+
+}
---
+++ MarkdownScanner/ScannerOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+
+namespace MarkdownScanner
+{
+    public class ScannerOptions
+    {
+        [Option("source", Required=true, HelpText="Specify the source folder for markdown documents")]
+        public string SourcePath {get;set;}
+
+        [Option('r', "recursive", DefaultValue=true, HelpText="Indicate that the scan should happen recursively on folders within source")]
+        public bool Recursive {get;set;}
+
+        [Option("extension", DefaultValue=".md", HelpText="The file extension for markdown files, defaults to \".md\"")]
+        public string FileExtension {get;set;}
+
+        [Option("verbose", DefaultValue=false, HelpText="Verbose output")]
+        public bool Verbose {get;set;}
+
+        public string UsageText 
+        {
+            get 
+            {
+                return HelpText.AutoBuild(this);
+            }
+        }
+    }
+}
+
