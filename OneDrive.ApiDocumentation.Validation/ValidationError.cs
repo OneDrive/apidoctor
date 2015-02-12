6e1c2c2e15d506416b3fde7a7d844624ed093100
daron spektor <dspektor@microsoft.com>
2015-02-11 23:39:55 -0800

    supporting validation for empty arrays

--- OneDrive.ApiDocumentation.Validation/ValidationError.cs
+++ OneDrive.ApiDocumentation.Validation/ValidationError.cs
@@ -14,6 +14,7 @@
         JsonErrorObject,
         MissingCollectionProperty,
         CollectionArrayEmpty,
+        CollectionArrayNotEmpty,
         RequiredPropertiesMissing,
         AdditionalPropertyDetected,
         ExpectedTypeDifferent,
