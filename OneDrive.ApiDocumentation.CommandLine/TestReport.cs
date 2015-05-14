2cbfc60bc877e792e37cd082b43fa60f69db3777
Ryan Gregg <rgregg@users.noreply.github.com>
2015-05-14 16:44:47 -0700

    Add missing file.

---
+++ OneDrive.ApiDocumentation.CommandLine/TestReport.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDrive.ApiDocumentation.ConsoleApp
+{
+    public static class TestReport
+    {
+        private const string TestFrameworkName = "apidocs";
+        private static AppVeyor.BuildWorkerApi BuildWorkerApi { get { return Program.BuildWorker; } }
+        private static Dictionary<string, long> TestStartTimes = new Dictionary<string, long>();
+
+        public static async Task StartTestAsync(string testName, string filename = null)
+        {
+            FancyConsole.WriteLine();
+            FancyConsole.Write(FancyConsole.ConsoleHeaderColor, "Starting test: ");
+            FancyConsole.Write(FancyConsole.ConsoleDefaultColor, testName);
+
+            if (null != filename)
+            {
+                FancyConsole.Write(" [{0}]", filename);
+            }
+            FancyConsole.WriteLine();
+
+            await BuildWorkerApi.RecordTestAsync(testName, TestFrameworkName, outcome: AppVeyor.TestOutcome.Running, filename: filename);
+
+            TestStartTimes[testName] = DateTimeOffset.Now.Ticks;
+        }
+
+
+        public static async Task FinishTestAsync(string testName, AppVeyor.TestOutcome outcome, string message = null, string filename = null, string stdErr = null)
+        {
+            var endTime = DateTimeOffset.Now.Ticks;
+            TimeSpan duration;
+            long startTime;
+            if (TestStartTimes.TryGetValue(testName, out startTime))
+            {
+                duration = new TimeSpan(endTime - startTime);
+            }
+            else
+            {
+                duration = TimeSpan.Zero;
+            }
+
+            FancyConsole.Write("Test {0} complete.", testName);
+
+            switch (outcome)
+            {
+                case AppVeyor.TestOutcome.Failed:
+                    FancyConsole.Write(ConsoleColor.Red, " Failed: {0}", message);
+                    break;
+                case AppVeyor.TestOutcome.Passed:
+                    FancyConsole.Write(ConsoleColor.Green, " Passed: {0}", message);
+                    break;
+                default:
+                    FancyConsole.Write(" {0}: {1}", outcome, message);
+                    break;
+            }
+
+            FancyConsole.WriteLine(" duration: {0}", duration);
+
+            await BuildWorkerApi.RecordTestAsync(testName, TestFrameworkName, outcome: outcome, durationInMilliseconds: (long)duration.TotalMilliseconds, errorMessage: message, filename: filename, stdErr: stdErr);
+        }
+
+    }
+}
