7c3e9c0a741800f60f433fab3fc59655dbc098d9
finsharp <mbuthiagrg@gmail.com>
2019-05-28 17:34:09 +0300

    Add tests to verify handling of illegal characters.

---
+++ ApiDoctor.Validation.UnitTests/DocFileTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiDoctor.Validation.Error;
+using NUnit.Framework;
+
+namespace ApiDoctor.Validation.UnitTests
+{
+    [TestFixture]
+    public class DocFileTests
+    {
+        [Test]
+        public void FileNameShouldNotContainerUnderscore()
+        {
+            DocSet docSet = new DocSet();
+            DocFileForTesting file = new DocFileForTesting(string.Empty, "test_file.md", "TestFile", docSet);
+            var issues = new IssueLogger();
+            file.Scan(string.Empty, issues);
+
+            Assert.IsNotEmpty(issues.Errors);
+            Assert.IsTrue(issues.Errors.Any(x => x.Code == ValidationErrorCode.IllegalCharacterInFileName));
+        }
+    }
+}
