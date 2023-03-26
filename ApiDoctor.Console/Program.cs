3bcf6ecf8c968ae66b11f932a2fa52e5e76a67ad
Millicent Achieng <achieng.milli@gmail.com>
2023-03-27 02:43:39 +0300

    Update boilerplate text for standard permissions tables

--- ApiDoctor.Console/Program.cs
+++ ApiDoctor.Console/Program.cs
@@ -2725,9 +2725,9 @@ namespace ApiDoctor.ConsoleApp
                                 // update boilerplate text
                                 if (boilerplateStartLine > -1 && foundPermissionTablesOrBlocks == 1 && !string.IsNullOrWhiteSpace(newPermissionFileContents))
                                 {
-                                    originalFileContents[boilerplateStartLine] = "Choose the permission marked as least privileged for this API." +
-                                        " Use a higher privileged permission only if your app requires it. For details about delegated and application permissions," +
-                                        " see [Permission types](/graph/permissions-overview#permission-types). To learn more about these permissions, see the [permissions reference](/graph/permissions-reference).";
+                                    originalFileContents[boilerplateStartLine] = "Choose the permission or permissions marked as least privileged for this API." +
+                                        " Use a higher privileged permission or permissions [only if your app requires it](/graph/permissions-overview#best-practices-for-using-microsoft-graph-permissions)." +
+                                        " For details about delegated and application permissions, see [Permission types](/graph/permissions-overview#permission-types). To learn more about these permissions, see the [permissions reference](/graph/permissions-reference).";
                                 }
                             }
 
